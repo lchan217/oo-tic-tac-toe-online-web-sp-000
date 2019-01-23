6582a8734fcf81f76816eafdd640c0e7b83b752c
Lizzie Chan <lchan217@gmail.com>
2019-01-23 20:33:37 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,11 @@
+class TicTacToe
+ def initialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " ")
+   @board = board
+ end
+ 
+ def board=(board)
+   @board = board
+ def board
+   @board
+ end
+end
